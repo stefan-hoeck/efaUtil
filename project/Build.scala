dee43874e362d11c2c72a089baf90ed5154833cc
efasckenoth <efasckenoth@gmail.com>
2012-11-01 13:23:41 +0100

    moved to Netbeans 7.2

--- project/Build.scala
+++ project/Build.scala
@@ -28,7 +28,7 @@ object Resolvers {
 }
 
 object Dependencies {
-  val nbV = "RELEASE71"
+  val nbV = "RELEASE72"
   val orgNb = "org.netbeans.api"
 
   val scalaSwing = "org.scala-lang" % "scala-swing" % "2.9.2" 
