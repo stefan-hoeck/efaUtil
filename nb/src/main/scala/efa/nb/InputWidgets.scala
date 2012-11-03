eb621eb9172c216788fea9f4708d68eee49d2b95
efasckenoth <efasckenoth@gmail.com>
2012-11-03 13:08:25 +0100

    helper methods for InputWidgets

--- nb/src/main/scala/efa/nb/InputWidgets.scala
+++ nb/src/main/scala/efa/nb/InputWidgets.scala
@@ -14,21 +14,6 @@ trait InputWidgetsFunctions {
   type StSET[A,B] = SET[A,State[B,Unit]]
   type VSET[A,B] = SET[A,ValRes[State[B,Unit]]]
 
-  def validate[A,B](v: Validator[A,B]): ValEET[A,B] =
-    eTrans.id[A] map (v run _ validation)
-
-  def success[A]: ValEET[A,A] = validate(Validators.dummy)
-
-  def lensed[A,B] (in: ValSET[B,B])(l: A @> B): VSET[A,A] =
-    in map (_ map (l := _ void)) contramap l.get
-
-  def lensedV[A,B] (in: VSET[B,B])(l: A @> B): VSET[A,A] = {
-    def nextSt (s: State[B,Unit]): State[A,Unit] =
-      init[A] >>= (a ⇒ l := s.exec (l get a)) void
-
-    in map (_ map nextSt) contramap l.get
-  }
-
   def checkBox[A] (b: CheckBox)(l: A @> Boolean): VSET[A,A] =
     lensed(values(b) >=> success)(l)
 
@@ -40,26 +25,46 @@ trait InputWidgetsFunctions {
     v: EndoVal[Int] = Validators.dummy[Int]
   )(l: A @> Int): VSET[A,A] = textIn[A,Int](t, v = v)(l)
 
+  def lensed[A,B] (in: ValSET[B,B])(l: A @> B): VSET[A,A] =
+    in map (_ map (l := _ void)) contramap l.get
+
+  def lensedV[A,B] (in: VSET[B,B])(l: A @> B): VSET[A,A] = {
+    def nextSt (s: State[B,Unit]): State[A,Unit] =
+      init[A] >>= (a ⇒ l := s.exec (l get a)) void
+
+    in map (_ map nextSt) contramap l.get
+  }
+
   def longIn[A](
     t: TextField,
     v: EndoVal[Long] = Validators.dummy[Long]
   )(l: A @> Long): VSET[A,A] = textIn[A,Long](t, v = v)(l)
+
+  def readVals[A:Read](
+    t: TextField,
+    f: A ⇒ String = (a: A) ⇒ a.toString
+  ): ValSET[A,A] = 
+    values(t) andThen validate(Read[A].validator) contramap f
+
+  def revalidate[A,B](v: Validator[A,B]): ValEET[ValRes[A],B] =
+    eTrans.id[ValRes[A]] map (_ flatMap (v run _ validation))
 
   def stringIn[A](
     t: TextField,
     v: EndoVal[String] = Validators.dummy[String]
   )(l: A @> String): VSET[A,A] = textIn[A,String](t, v = v)(l)
 
+  def success[A]: ValEET[A,A] = validate(Validators.dummy)
+
   def textIn[A,B:Read](
     t: TextField,
     v: EndoVal[B] = Validators.dummy[B],
     str: B ⇒ String = (b: B) ⇒ b.toString
-  )(l: A @> B): VSET[A,A] = {
-    def vtot: Validator[String,B] = Read[B].validator >=> v
-    def valSET = values(t) andThen validate(vtot) contramap str
+  )(l: A @> B): VSET[A,A] =
+    lensed(readVals(t, str) andThen revalidate(v))(l)
 
-    lensed(valSET)(l)
-  }
+  def validate[A,B](v: Validator[A,B]): ValEET[A,B] =
+    eTrans.id[A] map (v run _ validation)
 }
 
 object InputWidgets extends InputWidgets
