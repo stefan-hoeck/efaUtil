c6c1069c371e5dc5e3e74149a4a53f0b3156f830
gundi <hock@zhaw.ch>
2012-12-05 15:04:56 +0100

    added tests for efa.core.Level

---
+++ core/src/test/scala/efa/core/LevelTest.scala
@@ -0,0 +1,16 @@
+package efa.core
+
+import Efa._
+import org.scalacheck._, Prop._
+import scalaz._, Scalaz._
+
+object LevelTest extends Properties("Level") {
+  property("read") = Level.values ∀ (l ⇒ l.name.read[Level] ≟ l.success)
+
+  property("equal") = Level.values ∀ (l ⇒ l ≟ l)
+
+  property("notEqual") = Level.values ∀ (l1 ⇒
+    Level.values.filterNot (l1 ==) ∀ (l1 ≠ ))
+}
+
+// vim: set ts=2 sw=2 et:
