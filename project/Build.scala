163e6cb075064b0cd7e842c0044b4d53abedf130
stefan-hoeck <hock@zhaw.ch>
2016-07-14 05:57:09 +0200

    bumped version

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 object BuildSettings {
   val sv                = "2.11.8"
   val buildOrganization = "efa"
-  val buildVersion      = "0.2.5-SNAPSHOT"
+  val buildVersion      = "0.2.6-SNAPSHOT"
   val buildScalaVersion = sv
   val netbeansRepo      = "Netbeans" at "http://bits.netbeans.org/maven2/"
 
